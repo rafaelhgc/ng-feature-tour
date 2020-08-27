05f78ca3304141b759aa7d55515ccf95a2c01336
Rafael Costa <gomes.rafaelcosta@gmail.com>
2020-08-27 15:26:33 -0300

    get rect bounds

--- projects/ng-feature-tour/src/lib/ng-feature-tour.component.ts
+++ projects/ng-feature-tour/src/lib/ng-feature-tour.component.ts
@@ -7,7 +7,13 @@ import {
   Output,
   EventEmitter,
 } from '@angular/core';
-import { TourStep, TourEvent, EventEnum } from './ng-feature-tour.model';
+import {
+  TourStep,
+  TourEvent,
+  EventEnum,
+  StepBounds,
+  LensBounds,
+} from './ng-feature-tour.model';
 
 @Component({
   selector: 'ng-feature-tour',
@@ -44,8 +50,6 @@ export class NgFeatureTourComponent {
 
   currentStep: TourStep;
 
-  currentStepIndex: number;
-
   stepTrack: string[];
 
   constructor(private renderer: Renderer2) {
@@ -54,14 +58,25 @@ export class NgFeatureTourComponent {
     this.onAbort = new EventEmitter<TourEvent>();
     this.onFinish = new EventEmitter<TourEvent>();
     this.onChange = new EventEmitter<TourEvent>();
+    this.stepTrack = [];
   }
 
-  private scrollToTop(target: string): void {
+  private scrollToTop({ target }: TourStep): void {
     window.scrollTo(0, document.getElementById(target).offsetTop - 16);
   }
 
-  private getIndexFromTarget(target: string): number {
-    return this.steps.map(({ target }) => target).indexOf(target);
+  private getStepByTarget(target: string): TourStep {
+    return this.steps.filter((step) => step.target === target).pop();
+  }
+
+  private getIndexFromStep(search?: TourStep): number {
+    const step = search || this.currentStep;
+
+    if (!step) {
+      return -1;
+    }
+
+    return this.steps.map(({ target }) => target).indexOf(step.target);
   }
 
   private emitChangeEvent(target: string, event: EventEnum): void {
@@ -89,63 +104,134 @@ export class NgFeatureTourComponent {
     this.onChange.emit(tourEvent);
   }
 
-  private applyBounds(target: string): void {
-    const bounds = document.getElementById(target).getBoundingClientRect();
+  private getStepBounds(targetRect: DOMRect): StepBounds {
+    const { availHeight, availWidth } = screen;
+    const margin = 32;
+    const maxWidth = availHeight / 2;
+    const stepRect = this.stepRef.nativeElement.getBoundingClientRect();
+
+    let y: number;
+    let x: number;
+    let yPosition: 'to-bottom' | 'to-top';
+    let xPosition: 'to-left' | 'to-right';
+
+    // axis y
+    if (targetRect.y > availHeight - (targetRect.y + targetRect.height)) {
+      yPosition = 'to-top';
+      y = targetRect.y - stepRect.height - margin;
+    } else {
+      yPosition = 'to-bottom';
+      y = targetRect.y + targetRect.height + margin;
+    }
+
+    // axis x
+    if (targetRect.x > availWidth / 2) {
+      xPosition = 'to-right';
+      x = targetRect.x + targetRect.width - stepRect.width;
+    } else {
+      xPosition = 'to-left';
+      x = targetRect.x;
+    }
+
+    return { yPosition, xPosition, y, x, maxWidth };
+  }
+
+  private applyStepBounds(rect: DOMRect): void {
+    const { x, y, maxWidth, yPosition, xPosition } = this.getStepBounds(rect);
     const step = this.stepRef.nativeElement;
+
+    this.renderer.setStyle(step, 'transform', `translate(${x}px, ${y}px)`);
+    this.renderer.setStyle(step, 'max-width', `${maxWidth}px`);
+    this.renderer.removeClass(step, 'to-top');
+    this.renderer.removeClass(step, 'to-left');
+    this.renderer.removeClass(step, 'to-right');
+    this.renderer.removeClass(step, 'to-bottom');
+    this.renderer.addClass(step, yPosition);
+    this.renderer.addClass(step, xPosition);
+  }
+
+  private getLensBounds({ y, x, width, height }: DOMRect): LensBounds {
+    return { y, x, width, height };
+  }
+
+  private applyLensBounds(rect: DOMRect): void {
+    const { x, y, width, height } = this.getLensBounds(rect);
     const lens = this.lensRef.nativeElement;
 
-    this.renderer.setStyle(step, 'top', `${bounds.bottom + 32}px`);
-    this.renderer.setStyle(step, 'left', `${bounds.left}px`);
-    this.renderer.setStyle(lens, 'top', `${bounds.top}px`);
-    this.renderer.setStyle(lens, 'left', `${bounds.left}px`);
-    this.renderer.setStyle(lens, 'width', `${bounds.width}px`);
-    this.renderer.setStyle(lens, 'height', `${bounds.height}px`);
+    this.renderer.setStyle(lens, 'transform', `translate(${x}px, ${y}px)`);
+    this.renderer.setStyle(lens, 'width', `${width}px`);
+    this.renderer.setStyle(lens, 'height', `${height}px`);
+  }
+
+  private applyBounds({ target }: TourStep): void {
+    const targetRect = document.getElementById(target).getBoundingClientRect();
+
+    this.applyStepBounds(targetRect);
+    this.applyLensBounds(targetRect);
   }
 
   private setFocus(): void {
-    this.scrollToTop(this.currentStep.target);
-    this.applyBounds(this.currentStep.target);
+    this.scrollToTop(this.currentStep);
+
+    setTimeout(() => {
+      this.applyBounds(this.currentStep);
+    }, 0);
   }
 
-  private changeStep(event: EventEnum): void {
-    this.currentStep = this.steps[this.currentStepIndex];
-    this.stepTrack.push(this.currentStep.target);
-    this.setFocus();
+  private changeStep(event: EventEnum, step?: TourStep): void {
+    this.currentStep = step;
+    this.stepTrack.push(step.target);
     this.emitChangeEvent(this.currentStep.target, event);
+    this.setFocus();
   }
 
   start() {
-    this.stepTrack = [];
-    this.initialStepTarget = this.initialStepTarget || this.steps[0].target;
-    this.currentStepIndex = this.getIndexFromTarget(this.initialStepTarget);
-    this.changeStep(EventEnum.Start);
+    let initialStep: TourStep;
+
+    if (this.initialStepTarget) {
+      initialStep = this.getStepByTarget(this.initialStepTarget);
+    } else {
+      initialStep = this.steps[0];
+    }
+
+    this.changeStep(EventEnum.Start, initialStep);
   }
 
   next(): void {
-    this.currentStepIndex++;
-    this.changeStep(EventEnum.Next);
+    const stepIndex = this.getIndexFromStep();
+    const nextStep = this.steps[stepIndex + 1];
+
+    this.changeStep(EventEnum.Next, nextStep);
   }
 
   previous(): void {
-    this.currentStepIndex--;
-    this.changeStep(EventEnum.Previous);
+    const stepIndex = this.getIndexFromStep();
+    const previousStep = this.steps[stepIndex - 1];
+
+    this.changeStep(EventEnum.Previous, previousStep);
   }
 
   finish(): void {
     this.emitChangeEvent(this.currentStep.target, EventEnum.Finish);
     this.currentStep = null;
+    this.stepTrack = [];
   }
 
   abort(): void {
     this.emitChangeEvent(this.currentStep.target, EventEnum.Abort);
     this.currentStep = null;
+    this.stepTrack = [];
   }
 
   isLastStep(): boolean {
-    return this.currentStepIndex === this.steps.length - 1;
+    const currentStepIndex = this.getIndexFromStep();
+
+    return this.currentStep && currentStepIndex === this.steps.length - 1;
   }
 
   isFirstStep(): boolean {
-    return this.currentStepIndex === 0;
+    const currentStepIndex = this.getIndexFromStep();
+
+    return currentStepIndex === 0;
   }
 }
--- projects/ng-feature-tour/src/lib/ng-feature-tour.model.ts
+++ projects/ng-feature-tour/src/lib/ng-feature-tour.model.ts
@@ -17,3 +17,18 @@ export interface TourEvent {
   stepTrack: string[];
   currentStep: string;
 }
+
+export interface StepBounds {
+  yPosition: 'to-bottom' | 'to-top';
+  xPosition: 'to-left' | 'to-right';
+  x: number;
+  y: number;
+  maxWidth: number;
+}
+
+export interface LensBounds {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
